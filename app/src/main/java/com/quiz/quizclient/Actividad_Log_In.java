174fce9995ab76112dbd3409389864b2744d5a1e
Guillermo <guillermoomg@gmail.com>
2021-04-27 17:42:56 +0200

    probando commit

--- app/src/main/java/com/quiz/quizclient/Actividad_Log_In.java
+++ app/src/main/java/com/quiz/quizclient/Actividad_Log_In.java
@@ -2,18 +2,14 @@ package com.quiz.quizclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import com.quiz.quizclient.R;
 
-public class Actividad_Principal extends AppCompatActivity {
+public class Actividad_Log_In extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actividad_principal);
+        setContentView(R.layout.actividad_log_in);
     }
-
 }
